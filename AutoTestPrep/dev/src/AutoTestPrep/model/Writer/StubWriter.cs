7125fd2779e370ab81075b84fb7a4bc88e212bbb
CountrySideEngineer <deep-sleeper@nifty.com>
2021-04-24 16:40:38 +0900

    Add class to write stub code into text file. -Add interface class, IWriter, and concrete class, StubWriter which inherits IWriter class. -Add template and its class.

---
+++ AutoTestPrep/dev/src/AutoTestPrep/model/Writer/StubWriter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using AutoTestPrep.Model.Tempaltes;
+
+namespace AutoTestPrep.Model.Writer
+{
+	/// <summary>
+	/// Class to write code of function stub, or mock into text file.
+	/// </summary>
+	public class StubWriter : IWriter
+	{
+		public void Write(string path, object parameter)
+		{
+			Parameter param = (Parameter)parameter;
+			var template = new CFunctionStubTemplate(param);
+
+			using (var stream = new StreamWriter(path, false, Encoding.Unicode))
+			{
+				stream.Write(template.TransformText());
+			}
+		}
+	}
+}
