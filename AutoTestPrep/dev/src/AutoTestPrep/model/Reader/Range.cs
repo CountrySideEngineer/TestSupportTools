f66c3da1dad470bca2329f502442ffbddb7606f4
CountrySideEngineer <deep-sleeper@nifty.com>
2021-04-29 16:05:56 +0900

    Update class to model class contains range of data. -Update Range class by adding default and copy constructor.

--- AutoTestPrep/dev/src/AutoTestPrep/model/Reader/Range.cs
+++ AutoTestPrep/dev/src/AutoTestPrep/model/Reader/Range.cs
@@ -8,6 +8,29 @@ namespace AutoTestPrep.Model.Reader
 {
 	public class Range
 	{
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public Range()
+		{
+			this.StartRow = 0;
+			this.StartColumn = 0;
+			this.RowCount = 0;
+			this.ColumnCount = 0;
+		}
+
+		/// <summary>
+		/// Copy constructor.
+		/// </summary>
+		/// <param name="src">Base Range object.</param>
+		public Range(Range src)
+		{
+			this.StartRow = src.StartRow;
+			this.StartColumn = src.StartColumn;
+			this.RowCount = src.RowCount;
+			this.ColumnCount = src.ColumnCount;
+		}
+
 		public int StartRow { get; set; }
 
 		public int StartColumn { get; set; }
