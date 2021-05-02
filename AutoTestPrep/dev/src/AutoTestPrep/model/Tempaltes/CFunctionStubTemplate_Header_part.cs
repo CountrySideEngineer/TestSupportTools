42b471d0782a1bcb84cf8ae65800d4f4033012a4
CountrySideEngineer <deep-sleeper@nifty.com>
2021-05-02 15:50:20 +0900

    Add new template class that of stub header.

---
+++ AutoTestPrep/dev/src/AutoTestPrep/model/Tempaltes/CFunctionStubTemplate_Header_part.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTestPrep.Model.Tempaltes
+{
+	partial class CFunctionStubTemplate_Header
+	{
+		/// <summary>
+		/// Functoin information.
+		/// </summary>
+		public Function Function { get; set; }
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="function">Function data.</param>
+		public CFunctionStubTemplate_Header(Function function)
+		{
+			this.Function = function;
+		}
+	}
+}
