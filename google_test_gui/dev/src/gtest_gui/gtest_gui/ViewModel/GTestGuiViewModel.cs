bced80adf82df0ddd50dfd1d0d2535d9eef1baa1
CountrySideEngineer <deep-sleeper@nifty.com>
2021-05-05 12:30:38 +0900

    Add view model classes. -Add ViewModelBase class, all view model class base. -Add GTestGuiViewModel class, view model of main window, MainWindow.xaml. -Update MainWindow.xaml setting view model namespace and setup property binding.

---
+++ google_test_gui/dev/src/gtest_gui/gtest_gui/ViewModel/GTestGuiViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gtest_gui.ViewModel
+{
+	/// <summary>
+	/// Main view model class of gtest_gui application.
+	/// </summary>
+	public class GTestGuiViewModel : ViewModelBase
+	{
+		/// <summary>
+		/// Test file path field.
+		/// </summary>
+		protected string _testFilePath;
+
+		/// <summary>
+		/// Field of which a test can run or not.
+		/// </summary>
+		protected bool _canRunTest;
+
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public GTestGuiViewModel()
+		{
+			this.TestFilePath = string.Empty;
+			this.CanRunTest = false;
+		}
+
+		/// <summary>
+		/// Test file path.
+		/// </summary>
+		public string TestFilePath
+		{
+			get
+			{
+				return this._testFilePath;
+			}
+			set
+			{
+				this._testFilePath = value;
+				this.RaisePropertyChanged("TestFilePath");
+			}
+		}
+
+		/// <summary>
+		/// Gets a values indicating whether the tests listed on and selected by user can run or not.
+		/// </summary>
+		public bool CanRunTest
+		{
+			get
+			{
+				return this._canRunTest;
+			}
+			set
+			{
+				this._canRunTest = value;
+				this.RaisePropertyChanged("CanRunTest");
+			}
+		}
+	}
+}
