2f44fde034b0ae2caa3378462379835aebe75235
CountrySideEngineer <deep-sleeper@nifty.com>
2021-06-20 07:42:23 +0900

    Add a class event argument class. -Add a class about "selected state changed" event which inherits EventArgs class.

---
+++ AutoTestPrep/dev/src/AutoTestPrep/model/EventArgs/SelectedStateChangedEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTestPrep.Model.EventArgs
+{
+	public class SelectedStateChangedEventArgs : System.EventArgs
+	{
+		public SelectedStateChangedEventArgs(int oldSelected, int newSelected)
+		{
+			this.OldSelectedIndex = oldSelected;
+			this.NewSelectedIndex = newSelected;
+		}
+
+		public int OldSelectedIndex;
+
+		public int NewSelectedIndex;
+	}
+}
