75406b0e1058f8fd500a015711913b283507344f
CountrySideEngineer <deep-sleeper@nifty.com>
2021-07-08 22:26:16 +0900

    Update views. -Add view to input header files stub source and header files should include.

---
+++ AutoTestPrep/dev/src/AutoTestPrep/ViewModel/StubHeaderInformationInputViewModel.cs
@@ -0,0 +1,34 @@
+﻿using AutoTestPrep.Model.InputInfos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTestPrep.ViewModel
+{
+	public class StubHeaderInformationInputViewModel : HeaderInformationInputViewModel
+	{
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public StubHeaderInformationInputViewModel() : base() { }
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="index">Index of view model.</param>
+		public StubHeaderInformationInputViewModel(int index) : base(index) { }
+
+		/// <summary>
+		/// Set input header file names to property "StubIncludeXxxxFiles" property of 
+		/// TestDataInfo object.
+		/// </summary>
+		/// <param name="testDataInfo">Object to set header file names.</param>
+		public override void SetupTestInfomation(ref TestDataInfo testDataInfo)
+		{
+			testDataInfo.StubIncludeStandardHeaderFiles = this.ToEnumrable(this.StandartHeader);
+			testDataInfo.StubIncludeUserHeaderFiles = this.ToEnumrable(this.UserHeader);
+		}
+	}
+}
