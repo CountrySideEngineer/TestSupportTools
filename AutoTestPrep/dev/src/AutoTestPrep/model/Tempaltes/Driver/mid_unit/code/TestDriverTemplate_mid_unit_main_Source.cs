a122cb9ffee2377482b65af08b0d801ba299f80f
CountrySideEngineer <deep-sleeper@nifty.com>
2021-09-05 08:26:21 +0900

    Add new template file for test. Add template of test driver using mid_unit test framework.

---
+++ AutoTestPrep/dev/src/AutoTestPrep/model/Tempaltes/Driver/mid_unit/code/TestDriverTemplate_mid_unit_main_Source.cs
@@ -0,0 +1,36 @@
+﻿using AutoTestPrep.Model.InputInfos;
+using AutoTestPrep.Model.Tempaltes.Driver.min_unit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTestPrep.Model.Tempaltes.Driver.mid_unit
+{
+	public partial class TestDriverTemplate_mid_unit_main_Source : TestDriverTemplate_min_unit_main_Source
+	{
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		protected TestDriverTemplate_mid_unit_main_Source() : base() { }
+
+		/// <summary>
+		/// Constructor with argument about test.
+		/// </summary>
+		/// <param name="test">Test data</param>
+		/// <param name="testDataInfo">Test data information.</param>
+		public TestDriverTemplate_mid_unit_main_Source(Test test, TestDataInfo testDataInfo)
+			: base(test, testDataInfo)
+		{ }
+
+		/// <summary>
+		/// Constructor with argument about test.
+		/// </summary>
+		/// <param name="tests">Collection of test.</param>
+		/// <param name="testDataInfo">Test data information.</param>
+		public TestDriverTemplate_mid_unit_main_Source(IEnumerable<Test> tests, TestDataInfo testDataInfo)
+			: base(tests, testDataInfo) 
+		{ }
+	}
+}
