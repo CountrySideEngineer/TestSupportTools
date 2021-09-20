8e8ccc1d3bfb507bfefc2bc07febe86e53bcdef6
CountrySideEngineer <deep-sleeper@nifty.com>
2021-09-20 18:42:29 +0900

    Add solution and project to develop test parser SDK.

---
+++ AutoTestPrep/dev/src/TestParser.SDK/Test/Function.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestParser.Test.Data
+{
+	public class Function : Parameter
+	{
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public Function() : base()
+		{
+			this.Arguments = null;
+			this.SubFunctions = null;
+		}
+
+		/// <summary>
+		/// Copy constructor
+		/// </summary>
+		/// <param name="src"></param>
+		public Function(Function src) : base(src)
+		{
+			if (src is Function)
+			{
+				this.SubFunctions = new List<Function>(src.SubFunctions);
+				this.Arguments = new List<Parameter>(src.Arguments);
+			}
+		}
+
+		/// <summary>
+		/// List of sub functions.
+		/// </summary>
+		public IEnumerable<Function> SubFunctions { get; set; }
+
+		/// <summary>
+		/// List of  arguments.
+		/// </summary>
+		public IEnumerable<Parameter> Arguments { get; set; }
+
+		/// <summary>
+		/// Create string of funtion definition.
+		/// </summary>
+		/// <returns>Function definition in string.</returns>
+		public override string ToString()
+		{
+			var toString = base.ToString();
+			toString += "(";
+			try
+			{
+				bool isTop = true;
+				foreach (var argument in this.Arguments)
+				{
+					if (!isTop)
+					{
+						toString += ", ";
+					}
+					toString += argument.ToString();
+					isTop = false;
+				}
+			}
+			catch (NullReferenceException)
+			{
+				//No argumetn -> Skip!
+			}
+			toString += ")";
+
+			return toString;
+		}
+
+		/// <summary>
+		/// Returns whether the function will return any value or not.
+		/// </summary>
+		/// <returns>
+		/// Returns true if the function will return any value, 
+		/// otherwise return false.
+		/// </returns>
+		public bool HasReturn()
+		{
+			bool hasReturn = false;
+			if ((("void").Equals(this.DataType.ToLower(), StringComparison.Ordinal)) &&
+				(PointerNum <= 0))
+			{
+				//A case that the data type without pointer means the function returns no value via return value.
+				hasReturn = false;
+			}
+			else
+			{
+				hasReturn = true;
+			}
+			return hasReturn;
+		}
+	}
+}
---
+++ AutoTestPrep/dev/src/TestParser.SDK/Test/TestCase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestParser.Test.Data
+{
+	/// <summary>
+	/// Test data model class.
+	/// </summary>
+	public class TestCase
+	{
+		public string Id { get; set; }
+
+		/// <summary>
+		/// Input information of a test case.
+		/// </summary>
+		public IEnumerable<TestData> Input { get; set; }
+
+		/// <summary>
+		/// Expectation of a test case.
+		/// </summary>
+		public IEnumerable<TestData> Expects { get; set; }
+	}
+}
