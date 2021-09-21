2a58565bc9e53bb15854ec4992c0c4cb7aea1493
CountrySideEngineer <deep-sleeper@nifty.com>
2021-09-22 07:00:35 +0900

    Add project. Add project to develop a library to generate stub code.

---
+++ AutoTestPrep/dev/src/StubCodeWriter/Writer/StubCodeWriter.cs
@@ -0,0 +1,37 @@
+﻿using CodeWriter;
+using CodeWriter.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWriter.Writer
+{
+	public class StubCodeWriter : ICodeWriter
+	{
+		/// <summary>
+		/// Write stub code into a file specified by <para>path</para>.
+		/// </summary>
+		/// <param name="path">Path of file to output </param>
+		/// <param name="data">Data about stub.</param>
+		public virtual void Write(string path, WriteData data)
+		{
+			using (Stream stream = new FileStream(path, FileMode.Create, FileAccess.Write))
+			{
+				this.Write(stream, data);
+			}
+		}
+
+		/// <summary>
+		/// Write stub code into a stream specified by <para>stream</para>.
+		/// </summary>
+		/// <param name="stream">Stream to output data.</param>
+		/// <param name="data">Data about stub.</param>
+		public virtual void Write(Stream stream, WriteData data)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
