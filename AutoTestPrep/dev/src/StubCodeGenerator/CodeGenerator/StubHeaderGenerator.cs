fcd15bfd085bfd0b45b9ae96990777a83796a999
CountrySideEngineer <deep-sleeper@nifty.com>
2021-09-29 07:11:45 +0900

    Add stub header generator class.

---
+++ AutoTestPrep/dev/src/StubCodeGenerator/CodeGenerator/StubHeaderGenerator.cs
@@ -0,0 +1,28 @@
+﻿using CodeGenerator.Data;
+using CodeGenerator.Stub.Template;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGenerator.Stub
+{
+	public class StubHeaderGenerator : StubCodeGenerator
+	{
+		/// <summary>
+		/// Create template class.
+		/// </summary>
+		/// <param name="writeData">Write data.</param>
+		/// <returns>Template class to create stub header file.</returns>
+		protected override StubTemplate CreateTemplate(WriteData writeData)
+		{
+			var template = new StubHeaderTemplate()
+			{
+				ParentFunction = writeData.Test.Target,
+				Config = writeData.CodeConfig
+			};
+			return template;
+		}
+	}
+}
