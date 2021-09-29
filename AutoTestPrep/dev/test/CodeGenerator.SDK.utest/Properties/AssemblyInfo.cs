5982e2c0f3bc8160eb9521d93e60b00f95f037b7
CountrySideEngineer <deep-sleeper@nifty.com>
2021-09-29 19:46:31 +0900

    Add test of CodeConfiguration, in CodeGenerator.SDK project.

---
+++ AutoTestPrep/dev/test/CodeGenerator.SDK.utest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CodeGenerator.SDK.utest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CodeGenerator.SDK.utest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("918cc46c-c702-474e-b00c-35ca5e896674")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
