898e367c2ecd41f8147bc3156ae20b5e44d5e96a
CountrySideEngineer <deep-sleeper@nifty.com>
2021-10-03 16:00:35 +0900

    Add project to generate code of test driver. Add new project to generate unit test driver code using google test framework.

---
+++ AutoTestPrep/dev/src/GoogleTestDriverCodeGenerator/Template/Code/GoogleTestTemplate_code.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestParser.Target;
+
+namespace CodeGenerator.TestDriver.Template
+{
+    public partial class GoogleTestTemplate
+    {
+        /// <summary>
+        /// Create a unit test class name.
+        /// </summary>
+        /// <param name="function">Target function data</param>
+        /// <returns>Unite test class name.</returns>
+        protected virtual string TestClassName(Function function)
+		{
+            string testClassName = string.Empty;
+            testClassName = $"{function.Name}_utest";
+
+            return testClassName;
+		}
+
+        /// <summary>
+        /// Craete test function name for unit test.
+        /// </summary>
+        /// <param name="caseNumber">Test case number.</param>
+        /// <param name="function">Target function data.</param>
+        /// <returns>Test function name for unit test.</returns>
+        protected virtual string TestCaseMethodName(int caseNumber, Function function)
+		{
+            string testCaseMethodName = string.Empty;
+            testCaseMethodName = $"{function.Name}_utest_{caseNumber.ToString("D3")}";
+            return testCaseMethodName;
+		}
+    }
+}
