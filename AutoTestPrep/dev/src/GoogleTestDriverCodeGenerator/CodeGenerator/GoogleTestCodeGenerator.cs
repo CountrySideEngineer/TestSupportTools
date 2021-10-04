75d3080cf875f8daea0980b96cecedb21c5a27ff
CountrySideEngineer <deep-sleeper@nifty.com>
2021-10-04 14:39:28 +0900

    Add code generator for google test.

---
+++ AutoTestPrep/dev/src/GoogleTestDriverCodeGenerator/CodeGenerator/GoogleTestCodeGenerator.cs
@@ -0,0 +1,30 @@
+﻿using CodeGenerator.Data;
+using CodeGenerator.TestDriver.Template;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGenerator.TestDriver.GoogleTest
+{
+	public abstract class GoogleTestCodeGenerator : ICodeGenerator
+	{
+		/// <summary>
+		/// Generate stub code.
+		/// </summary>
+		/// <param name="data">Data for code.</param>
+		/// <returns>Generated stub code.</returns>
+		public string Generate(WriteData data)
+		{
+			var template = this.CreateTemplate(data);
+			return template.TransformText();
+		}
+
+		/// <summary>
+		/// Abstract method to create template.
+		/// </summary>
+		/// <returns>StubTemplate class.</returns>
+		protected abstract GoogleTestTemplate CreateTemplate(WriteData writeData);
+	}
+}
