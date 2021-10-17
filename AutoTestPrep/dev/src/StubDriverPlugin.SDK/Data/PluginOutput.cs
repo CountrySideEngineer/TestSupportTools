bde23b07f8e31741f0c7f2f88eaf07bbf85b0b70
CountrySideEngineer <deep-sleeper@nifty.com>
2021-10-17 14:46:38 +0900

    Add project and solution to develop plugins. -Add solution to develop plugins. -Add project to develop plugin and its SDK as dll.

---
+++ AutoTestPrep/dev/src/StubDriverPlugin.SDK/Data/PluginOutput.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StubDriverPlugin.Data
+{
+	public class PluginOutput
+	{
+		/// <summary>
+		/// Message about plugin
+		/// </summary>
+		string Message { get; set; }
+
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public PluginOutput()
+		{
+			this.Message = string.Empty;
+		}
+
+		/// <summary>
+		/// Constructor with message.
+		/// </summary>
+		/// <param name="message">Message about result of plugin.</param>
+		public PluginOutput(string message)
+		{
+			this.Message = message;
+		}
+
+		/// <summary>
+		/// Copy constructor.
+		/// </summary>
+		/// <param name="src">Copy source object.</param>
+		public PluginOutput(PluginOutput src)
+		{
+			this.Message = src.Message;
+		}
+
+	}
+}
