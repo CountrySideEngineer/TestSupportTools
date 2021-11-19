fea924e9f8fa75116739c0137edbff5890992ce9
CountrySideEngineer <deep-sleeper@nifty.com>
2021-11-20 07:02:08 +0900

    Add exception class for code test parser.

---
+++ AutoTestPrep/dev/src/TestParser.SDK/ParserException/TestParserException.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestParser.ParserException
+{
+	public class TestParserException : Exception
+	{
+		/// <summary>
+		/// Error code.
+		/// </summary>
+		public long ErrorCode { get; protected set; }
+
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public TestParserException() : base()
+		{
+			this.ErrorCode = -1;
+		}
+
+		public TestParserException(string message) : base(message)
+		{
+			this.ErrorCode = -1;
+		}
+
+		/// <summary>
+		/// Constructor with argument.
+		/// </summary>
+		/// <param name="message">Error message.</param>
+		/// <param name="innerException">Inner exception.</param>
+		public TestParserException(string message, Exception innerException) 
+			: base(message, innerException)
+		{
+			this.ErrorCode = -1;
+		}
+	}
+
+}
