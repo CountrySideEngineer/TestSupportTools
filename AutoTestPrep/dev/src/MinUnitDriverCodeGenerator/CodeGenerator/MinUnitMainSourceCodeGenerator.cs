692bf969e1eda07b1a1db0966a7d9fc8737d8b15
CountrySideEngineer <deep-sleeper@nifty.com>
2021-11-21 09:31:27 +0900

    Add assertion code to min unit code.

--- AutoTestPrep/dev/src/MinUnitDriverCodeGenerator/CodeGenerator/MinUnitMainSourceCodeGenerator.cs
+++ AutoTestPrep/dev/src/MinUnitDriverCodeGenerator/CodeGenerator/MinUnitMainSourceCodeGenerator.cs
@@ -2,6 +2,7 @@
 using CodeGenerator.TestDriver.Template;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,9 @@ namespace CodeGenerator.TestDriver.MinUnit
 		/// <returns>Template for test driver main function code using min_unit test framework.</returns>
 		protected override MinUnitTemplate CreateTemplate(WriteData writeData)
 		{
+			Debug.Assert(null != writeData.Test, $"{nameof(MinUnitMainSourceCodeGenerator)}.{nameof(CreateTemplate)}.{nameof(writeData)}");
+			Debug.Assert(null != writeData.Test.Target, $"{nameof(MinUnitMainSourceCodeGenerator)}.{nameof(CreateTemplate)}.{nameof(writeData)}.{nameof(writeData.Test)}");
+
 			var template = new MinUnitSourceMainTemplate()
 			{
 				TargetFunction = writeData.Test.Target
