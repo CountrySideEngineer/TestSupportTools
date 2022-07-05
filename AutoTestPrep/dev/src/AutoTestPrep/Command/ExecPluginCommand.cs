e4305192f6281586fbd23a3adfad51ed3f8b45b1
CountrySideEngineer <deep-sleeper@nifty.com>
2022-07-05 22:48:25 +0900

    Update logger. -Update logger to use a dll developed by other repository.

--- AutoTestPrep/dev/src/AutoTestPrep/Command/ExecPluginCommand.cs
+++ AutoTestPrep/dev/src/AutoTestPrep/Command/ExecPluginCommand.cs
@@ -76,11 +76,7 @@ namespace AutoTestPrep.Command
 				string logFilePath = this.GetLogFilePath();
 				using (var stream = new StreamWriter(logFilePath, false, Encoding.UTF8))
 				{
-					Logger.Level = Logger.LogLevel.All;
-					Logger.AddStream(stream);
-					Logger.INFO("Start logging!");
 					this._Execute(commandArg);
-					Logger.RemoveStream(stream);
 				}
 			}
 			catch (System.Exception ex)
