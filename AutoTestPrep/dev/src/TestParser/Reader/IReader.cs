601975043e36f1c629dc002a3a5c8b31dcbc30ec
CountrySideEngineer <deep-sleeper@nifty.com>
2022-07-18 10:00:29 +0900

    Update parser dll. -Add new class to read configuration from a file in XML format. -Add "interface" for reading configuration.

---
+++ AutoTestPrep/dev/src/TestParser/Reader/IReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestParse.Reader
+{
+	public interface IReader
+	{
+		object Read(string path);
+
+		object Read(TextReader stream);
+	}
+}
